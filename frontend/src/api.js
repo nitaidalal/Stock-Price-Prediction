846503dc0c7326dd234a13e058d49a1b666aece4
nitaidalal <dalalnitai7@gmail.com>
2025-11-25 17:50:37 +0530

    Backend URL added in frontend

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,4 +1,4 @@
-const BASE_URL = "http://localhost:8000";
+const BASE_URL = "https://stock-price-prediction-lcif.onrender.com";
 
 export async function getHistorical(ticker) {
   const res = await fetch(`${BASE_URL}/historical?ticker=${ticker}`);
